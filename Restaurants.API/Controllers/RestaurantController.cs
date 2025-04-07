8b9f75572f9fd20aea3b3eec190af87b42139e28
marwan abed <marwanabed453@gmail.com>
2025-04-07 05:28:09 +0200

    show the expected response in swagger

--- Restaurants.API/Controllers/RestaurantController.cs
+++ Restaurants.API/Controllers/RestaurantController.cs
@@ -4,7 +4,6 @@ using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantsById;
-using Restaurants.Application.Restaurants.Services;
 
 namespace Restaurants.API.Controllers
 {
@@ -13,10 +12,11 @@ namespace Restaurants.API.Controllers
     //if we want to make it manually we can remove the [apiController] and add the model.state.isvalid in the controller
     //also apiController add another parameters which is by default sending paramters [formBody]
     [Route("api/[controller]")]
-    public class RestaurantController (IRestaurantsServices restaurantsServices,IMediator mediator) : ControllerBase
+    public class RestaurantController (IMediator mediator) : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<RestaurantDto>))]
+        public async Task<ActionResult<IEnumerable<RestaurantDto>>> GetAll()
         {
             //var restaurants = await restaurantsServices.GetRestaurants();   
             var restaurants = await mediator.Send(new GetAllRestaurantsQuery());
@@ -24,7 +24,7 @@ namespace Restaurants.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<ActionResult<RestaurantDto>> GetById(int id)
         {
             //var restaurant = await restaurantsServices.GetRestaurantById(id);
             var restaurant = await mediator.Send(new GetRestaurantByIdQuery(id));
@@ -36,11 +36,31 @@ namespace Restaurants.API.Controllers
         }
 
         [HttpPost]
+         [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<IActionResult> Create(CreateRestaurantCommand createRestaurantCommand)
         {
             //var id = await restaurantsServices.CreateRestaurant(createRestaurantDto); 
             var id = await mediator.Send(createRestaurantCommand);
             return CreatedAtAction(nameof(GetById), new { id }, null);
         }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(int id)
+        {
+            //if (id != updateRestaurantCommand.Id)
+            //{
+            //    return BadRequest();
+            //}
+            ////await restaurantsServices.UpdateRestaurant(updateRestaurantDto);
+            //await mediator.Send(updateRestaurantCommand);
+            return NoContent();
+        }
+
+        //for delete we can put this too
+        //[ProducesResponseType(StatusCodes.Status204NoContent)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
     }
 }
