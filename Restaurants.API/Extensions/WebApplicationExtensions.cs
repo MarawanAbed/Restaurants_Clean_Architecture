b11bca18b4cc7c51cd65f3c47a5eeb2ed8d98bef
marwan abed <marwanabed453@gmail.com>
2025-07-20 09:12:01 +0300

    adding extension for presentation

---
+++ Restaurants.API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Restaurants.API.Middlewares;
+using Restaurants.Application.Extensions;
+using Restaurants.Infrastructure.Extensions;
+using Serilog;
+
+namespace Restaurants.API.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void AddPresentation(this WebApplicationBuilder builder)
+        {
+
+            //serlog
+            builder.Host.UseSerilog((context, services, configuration) =>
+            {
+                configuration
+                .ReadFrom.Configuration(context.Configuration);
+            });
+            builder.Services.AddScoped<ErrorHandlingMiddleware>();
+            builder.Services.AddScoped<ReqestTimeLoggingMiddleware>();
+            ///////////////////////////////////////////
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("bearerAuth",
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme. Example: \"{token}\"",
+                        Name = "Authorization",
+                        Scheme = "Bearer",
+                        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http //we put it over http so that not every time we call apikey i need to send it
+                    });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                    {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "bearerAuth"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            }
+            );
+
+        }
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,10 +1,10 @@
+using Restaurants.API.Extensions;
+using Restaurants.API.Middlewares;
+using Restaurants.Application.Extensions;
+using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
-using Restaurants.Application.Extensions;
 using Serilog;
-using Serilog.Formatting.Compact;
-using Restaurants.API.Middlewares;
-using Restaurants.Domain.Entities;
 namespace Restaurants.API
 {
     public class Program
@@ -12,49 +12,12 @@ namespace Restaurants.API
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            //serlog
-            builder.Host.UseSerilog((context, services, configuration) =>
-            {
-                configuration
-                .ReadFrom.Configuration(context.Configuration);
-            });
-            builder.Services.AddScoped<ErrorHandlingMiddleware>();
-            builder.Services.AddScoped<ReqestTimeLoggingMiddleware>();
             // Add services infrastructure
             builder.Services.AddInfrastructure(builder.Configuration);
             // Add services application
             builder.Services.AddApplication();
-            ///////////////////////////////////////////
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c=>
-            {
-                    c.AddSecurityDefinition("bearerAuth",
-                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-                        {
-                            Description = "JWT Authorization header using the Bearer scheme. Example: \"{token}\"",
-                            Name = "Authorization",
-                            Scheme="Bearer",
-                            Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http //we put it over http so that not every time we call apikey i need to send it
-                        });
-
-                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-                    {
-                    {
-                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-                        {
-                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                            {
-                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                                Id = "bearerAuth"
-                            }
-                        },
-                        new string[] {}
-                    }
-                });
-            }
-            );
+            // Add services presentation
+            builder.AddPresentation();
             var app = builder.Build();
 
             // Seed the database
